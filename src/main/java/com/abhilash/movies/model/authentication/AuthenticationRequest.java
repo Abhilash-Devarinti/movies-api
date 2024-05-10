335ac3ec6f08307b78081a68f9ece2e5f916c0dc
Abhilash-Devarinti <devarinti.a@northeastern.edu>
2024-05-09 23:59:55 -0500

    Implemented JWT Authentication

---
+++ src/main/java/com/abhilash/movies/model/authentication/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.abhilash.movies.model.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    String password;
+}
